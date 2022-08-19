738b2355618ef815e56a40872196fe60456237b3
Adam Yellen <6933682+AdamYellen@users.noreply.github.com>
2022-08-19 10:55:02 -0400

    🧹

--- script/setup.ps1
+++ script/setup.ps1
@@ -6,31 +6,40 @@
 # * NOTE: "winget" is not available during the lifetime of this script
 # * Preconditions: none
 
-function DoUnpinFromTaskbar([string]$appname) {
+function DoUnpinFromTaskbar([string]$appname)
+{
     $ErrorActionPreference = 'silentlycontinue'
-     ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() `
-      | Where-Object { $_.Name -eq $appname }).Verbs() `
-      | Where-Object { $_.Name.replace('&', '') -match 'Unpin from taskbar' } `
-      | ForEach-Object { $_.DoIt(); }
+    ((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() `
+        | Where-Object { $_.Name -eq $appname }).Verbs() `
+        | Where-Object { $_.Name.replace('&', '') -match 'Unpin from taskbar' } `
+        | ForEach-Object { $_.DoIt(); }
     $ErrorActionPreference = 'continue'
  }
 
- function DoPinToQuickAccess([string]$folder) {
+ function DoPinToQuickAccess([string]$folder)
+ {
     $ErrorActionPreference = 'silentlycontinue'
     $QuickAccess = New-Object -ComObject Shell.Application 
     $QuickAccess.Namespace("$folder").Self.InvokeVerb("pintohome")
     $ErrorActionPreference = 'continue'
  }
 
- function DoUnpinFromQuickAccess([string]$folder) {
+ function DoUnpinFromQuickAccess([string]$folder)
+ {
     $ErrorActionPreference = 'silentlycontinue'
     $QuickAccess = New-Object -ComObject Shell.Application 
     ($QuickAccess.Namespace("shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}").Items() | Where-Object {$_.Path -eq "$folder"}).InvokeVerb("unpinfromhome")
     $ErrorActionPreference = 'continue'
  }
  
+function IsWindows10()
+{
+    return([System.Environment]::OSVersion.Version.Build -lt 22000)
+}
+
 # Privacy
-if ($False) {
+if($False)
+{
     Write-Host "Configuring Privacy..." -ForegroundColor "Green"
 
     # # General: Don't let apps use advertising ID for experiences across apps: Allow: 1, Disallow: 0
@@ -192,7 +201,8 @@ if ($False) {
 }
 
 # Devices, Power, and Startup
-if ($True) {
+if($True)
+{
     Write-Host "Configuring Devices, Power, and Startup..." -ForegroundColor "Green"
 
     # Sound: Disable Startup Sound
@@ -214,7 +224,8 @@ if ($True) {
 }
 
 # Desktop, Explorer, Taskbar, and System Tray
-if ($True) {
+if($True)
+{
     Write-Host "Configuring Desktop, Explorer, Taskbar, and System Tray..." -ForegroundColor "Green"
 
     # Ensure necessary registry paths
@@ -264,7 +275,7 @@ if ($True) {
     $thisPCIconRegPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel"
     $thisPCRegValname = "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" 
     $item = Get-ItemProperty -Path $thisPCIconRegPath -Name $thisPCRegValname -ErrorAction SilentlyContinue 
-    if ($item)
+    if($item)
     {
         Set-ItemProperty  -Path $thisPCIconRegPath -Name $thisPCRegValname -Value 0  
     }
@@ -275,10 +286,12 @@ if ($True) {
 
     # Desktop: Microsoft Edge Icon
     # New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\EdgeUpdate" -Name "CreateDesktopShortcutDefault" -Value 0 -PropertyType DWORD | Out-Null
-    if (Test-Path -Path "$HOME\Desktop\Microsoft Edge.lnk") {
+    if(Test-Path -Path "$HOME\Desktop\Microsoft Edge.lnk")
+    {
         Remove-Item "$HOME\Desktop\Microsoft Edge.lnk"
     }
-    if (Test-Path -Path "C:\Users\Public\Desktop\Microsoft Edge.lnk") {
+    if(Test-Path -Path "C:\Users\Public\Desktop\Microsoft Edge.lnk")
+    {
         Remove-Item "C:\Users\Public\Desktop\Microsoft Edge.lnk"
     }
 
@@ -292,7 +305,8 @@ if ($True) {
 }
 
 # Default Windows Applications
-if ($True) {
+if($True)
+{
     Write-Host "Configuring Default Windows Applications..." -ForegroundColor "Green"
 
     # Uninstall Windows Media Player
@@ -304,20 +318,25 @@ if ($True) {
     #Set-ItemProperty "HKLM:\Software\Policies\Microsoft\Windows\CloudContent" "DisableConsumerAccountStateContent" 1 -Force
 
     # Uninstall pre-installed apps
-    if([System.Environment]::OSVersion.Version.Build -lt 22000) {
-        if (Test-Path "$HOME\.dotfiles\bin\remove-win10apps.ps1") {
+    if(IsWindows10)
+    {
+        if(Test-Path "$HOME\.dotfiles\bin\remove-win10apps.ps1")
+        {
             & "$HOME\.dotfiles\bin\remove-win10apps.ps1"
         }
     }
-    else {
-        if (Test-Path "$HOME\.dotfiles\bin\remove-win11apps.ps1") {
+    else
+    {
+        if(Test-Path "$HOME\.dotfiles\bin\remove-win11apps.ps1")
+        {
             & "$HOME\.dotfiles\bin\remove-win11apps.ps1"
         }
     }
 }
 
 # Start Menu
-if ($True) {
+if($True)
+{
     Write-Host "Configuring Start Menu (Don't press any keys)..." -ForegroundColor "Green"
 
     # Our blank template
@@ -346,11 +365,12 @@ if ($True) {
     $START_MENU_LAYOUT | Out-File $layoutFile -Encoding ASCII
 
     # Assign the start layout and force it to apply with "LockedStartLayout" at both the machine and user level
-    foreach ($regAlias in $regAliases){
+    foreach($regAlias in $regAliases)
+    {
         $basePath = $regAlias + ":\SOFTWARE\Policies\Microsoft\Windows"
         $keyPath = $basePath + "\Explorer" 
         If(!(Test-Path -Path $keyPath))
-        { 
+        {
             New-Item -Path $basePath -Name "Explorer" | Out-Null
         }
         Set-ItemProperty -Path $keyPath -Name "LockedStartLayout" -Value 1
@@ -364,7 +384,7 @@ if ($True) {
     Start-Sleep -s 5
 
     # Enable the ability to pin items again by disabling "LockedStartLayout"
-    foreach ($regAlias in $regAliases)
+    foreach($regAlias in $regAliases)
     {
         $basePath = $regAlias + ":\SOFTWARE\Policies\Microsoft\Windows"
         $keyPath = $basePath + "\Explorer" 
@@ -372,7 +392,7 @@ if ($True) {
     }
 
     # Hide Recently Added Items
-    if (!(Test-Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer")) {New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Type Folder | Out-Null}
+    if(!(Test-Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer")) {New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Type Folder | Out-Null}
     Set-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name "HideRecentlyAddedApps" -Value 1
 
     # Restart Explorer and delete the layout file
@@ -384,7 +404,8 @@ if ($True) {
 }
 
 # Accessibility and Ease of Use
-if ($True) {
+if($True)
+{
     Write-Host "Configuring Accessibility..." -ForegroundColor "Green"
 
     # Turn Off Windows Narrator Hotkey
@@ -413,7 +434,8 @@ if ($True) {
 }
 
 # Internet Explorer
-if ($True) {
+if($True)
+{
     Write-Host "Configuring Internet Explorer..." -ForegroundColor "Green"
 
     # Set home page to `about:blank` for faster loading
@@ -426,13 +448,15 @@ if ($True) {
     Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Internet Settings" "DisablePasswordCaching" 1
 
     # Disable IE11
-    if([System.Environment]::OSVersion.Version.Build -lt 22000) {
+    if(IsWindows10)
+    {
         Disable-WindowsOptionalFeature -FeatureName "Internet-Explorer-Optional-amd64" -Online -NoRestart -WarningAction SilentlyContinue | Out-Null
     }
 }
 
 # Disk Cleanup (CleanMgr.exe)
-if ($False) {
+if($False)
+{
     Write-Host "Configuring Disk Cleanup..." -ForegroundColor "Green"
 
     $diskCleanupRegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\"
@@ -469,7 +493,8 @@ if ($False) {
 }
 
 # PowerShell Console
-if ($True) {
+if($True)
+{
     Write-Host "Configuring Console..." -ForegroundColor "Green"
 
     # Make 'Source Code Pro' an available Console font
@@ -477,7 +502,8 @@ if ($True) {
 }
 
 # Development
-if ($True) {
+if($True)
+{
     Write-Host "Enable Development Settings..." -ForegroundColor "Green"
 
     # Enable Remote Desktop
@@ -490,33 +516,39 @@ if ($True) {
 }
 
 # WSL2
-if ($False) {
+if($False)
+{
     Write-Host "Installing Windows Subsystem for Linux..." -ForegroundColor "Green"
     wsl --install | Out-Null
 }
 
 # Home directory
-if ($True) {
+if($True)
+{
     Write-Host "Setup home directory..." -ForegroundColor "Green"
 
     # setup SSH
-    if (Test-Path -Path "$HOME\.ssh") {
+    if(Test-Path -Path "$HOME\.ssh")
+    {
         Remove-Item "$HOME\.ssh" -Force | Out-Null
     }
     New-Item -ItemType SymbolicLink -Path "$HOME\.ssh" -Target "$HOME\.dotfiles\ssh" | Out-Null
 
     # setup GPG
-    if (Test-Path -Path "$env:APPDATA\gnupg") {
+    if(Test-Path -Path "$env:APPDATA\gnupg")
+    {
         Remove-Item "$env:APPDATA\gnupg" -Force | Out-Null
     }
     New-Item -ItemType SymbolicLink -Path "$env:APPDATA\gnupg" -Target "$HOME\.dotfiles\gnupg" | Out-Null
-    if (Test-Path -Path "$HOME\.gnupg") {
+    if(Test-Path -Path "$HOME\.gnupg")
+    {
         Remove-Item "$HOME\.gnupg" -Force | Out-Null
     }
     New-Item -ItemType SymbolicLink -Path "$HOME\.gnupg" -Target "$HOME\.dotfiles\gnupg" | Out-Null
 
     # setup bin
-    if (Test-Path -Path "$HOME\.bin") {
+    if(Test-Path -Path "$HOME\.bin")
+    {
         Remove-Item "$HOME\.bin" -Force | Out-Null
     }
     New-Item -ItemType SymbolicLink -Path "$HOME\.bin" -Target "$HOME\.dotfiles\bin" | Out-Null
@@ -524,4 +556,5 @@ if ($True) {
     New-Item -Path "$HOME\Projects" -ItemType Directory -Force | Out-Null
 }
 
+# All done!
 Write-Host "Setup complete -- Returning to strap..." -ForegroundColor "Green"
--- script/strap-after-setup.ps1
+++ script/strap-after-setup.ps1
@@ -9,66 +9,93 @@ param($strap_op_uri)
 #   * Apps installed: scoop, winget
 #   * Folders linked to dotfiles repo: $HOME/.bin, $HOME/.gnupg, $env:APPDATA/gnupg, $HOME/.ssh
 
-function Check-Command($cmdname) {
+# TODO:
+# * Put common commands into a module
+# * Don't error out; allow the user to attempt to fix the issue
+# * Support unavailable biometrics in 1Password setup (add 1Password account directly on the CLI)
+# * Find the executable paths dynamically (1Password, Synctrazor/Syncthing)
+# * Consolidate remove-win*apps
+
+function CheckCommand
+{
+   param
+   (
+      [string]$cmdname
+   )
    return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
 }
 
-function UpdateStoreApps {
+function UpdateStoreApps
+{
    $wmiObj = Get-WmiObject -Namespace "root\cimv2\mdm\dmmap" -Class "MDM_EnterpriseModernAppManagement_AppManagement01"
    $wmiObj.UpdateScanMethod() | Out-Null
    Start-Sleep -Seconds 30
    Remove-Variable wmiObj    
 }
 
-function RefreshEnv {
+function RefreshEnv
+{
    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 }
 
-function AddToUserPath {
-   param (
+function AddToUserPath
+{
+   param
+   (
       [string]$folder
    )
-
-   # Write-Host "Adding $folder to PATH..." -ForegroundColor "Green"
    $currentEnv = [Environment]::GetEnvironmentVariable("Path", [EnvironmentVariableTarget]::User).Trim(";");
    $addedEnv = $currentEnv + ";$folder"
    $trimmedEnv = (($addedEnv.Split(';') | Select-Object -Unique) -join ";").Trim(";")
    [Environment]::SetEnvironmentVariable("Path", $trimmedEnv, [EnvironmentVariableTarget]::User)
 }
 
-function AddToUserPathAndRefresh {
-   param (
+function AddToUserPathAndRefresh
+{
+   param
+   (
       [string]$folder
    )
    AddToUserPath $folder
    RefreshEnv
 }
 
+function IsWindows10
+{
+   return([System.Environment]::OSVersion.Version.Build -lt 22000)
+}
+
 # Prompt the user
 Write-Host "!! IMPORTANT !!" -ForegroundColor "Red"
-Read-Host -Prompt "This script requires user input and interaction -- Press Return to continue after system is ready"
+Read-Host -Prompt "This script requires user input and GUI interaction -- Press Return to continue after system is ready"
 
 # Do we have winget yet?
-while (-Not (Check-Command -cmdname 'winget')) {
+while(-not (CheckCommand -cmdname 'winget'))
+{
    Write-Host "Trying to update the Microsoft Store to get winget..." -ForegroundColor "Green"
-   UpdateStoreApps
+   UpdateStoreApps   # has a built-in sleep
 }
 
 # Dependencies needed to continue
 Write-Host "Installing dependencies..." -ForegroundColor "Green"
-if (Check-Command -cmdname 'scoop') {
+if(CheckCommand -cmdname 'scoop')
+{
    scoop install 1password-cli 6>&1 | Out-Null
 }
-else {
+else
+{
    Write-Host "!! Exiting: Scoop not installed or not in path !!" -ForegroundColor "Red"
    Return 1
 }
-if (Check-Command -cmdname 'winget') {
+
+if(CheckCommand -cmdname 'winget')
+{
    winget install AgileBits.1Password --silent --accept-source-agreements --accept-package-agreements | Out-Null
    winget install GnuPG.GnuPG --silent --accept-source-agreements --accept-package-agreements | Out-Null
    winget install SyncTrayzor.SyncTrayzor --silent --accept-source-agreements --accept-package-agreements | Out-Null
 }
-else {
+else
+{
    Write-Host "!! Exiting: Winget not installed or not in path !!" -ForegroundColor "Red"
    Return 1
 }
@@ -82,15 +109,18 @@ Write-Host "Press Return to continue AFTER Windows Hello is setup with at least 
 Start-Sleep -Seconds 2
 Start-Process ms-settings:signinoptions
 Read-Host -Prompt "Is Windows Hello setup? Press Return to continue"
-if (-Not $strap_op_uri) {
+if(-not $strap_op_uri)
+{
    $strap_op_uri = Read-Host -Prompt "Please enter a 1Password signup URI or press Return to setup manually"
 }
 Write-Host "Press Return to continue AFTER 1Password is setup -- Sign in and enable CLI under Developer settings" -ForegroundColor "Red"
 Start-Sleep -Seconds 2
-if ($strap_op_uri) {
+if($strap_op_uri)
+{
    Start-Process -Filepath "$env:LOCALAPPDATA\1Password\app\8\1Password.exe" -Args "$strap_op_uri" -RSO "$env:LOCALAPPDATA/Temp/1pass1.txt" -RSE "$env:LOCALAPPDATA/Temp/1pass2.txt"
 }
-else {
+else
+{
    Start-Process -Filepath "$env:LOCALAPPDATA\1Password\app\8\1Password.exe" -RSO "$env:LOCALAPPDATA/Temp/1pass1.txt" -RSE "$env:LOCALAPPDATA/Temp/1pass2.txt"
 }
 Read-Host -Prompt "Is 1Password CLI enabled? Press Return to continue"
@@ -102,10 +132,13 @@ gpg --list-keys | Out-Null
 & "$HOME\.bin\extract-onepassword-secrets.ps1"
 
 # Import our secrets by pretending we're BASH
-if (Test-Path "$HOME\.secrets") {
-   Get-Content "$HOME\.secrets" | Select-String -NotMatch "^#" | ForEach-Object { 
+if(Test-Path "$HOME\.secrets")
+{
+   Get-Content "$HOME\.secrets" | Select-String -NotMatch "^#" | ForEach-Object
+   { 
       $array = $_[0].ToString().split("=")
-      if ($array[0] -and $array[1]) {
+      if($array[0] -and $array[1])
+      {
          # .secrets probably shouldn't export secrets, but remove it if it exists
          $exportArray = $array[0].ToString().split(" ")
          if(($exportArray[0] -eq "export") -and $exportArray[1]) {
@@ -115,7 +148,8 @@ if (Test-Path "$HOME\.secrets") {
       }
    }
 }
-else {
+else
+{
    Write-Host "!! Exiting: Failed to get secrets !!" -ForegroundColor "Red"
    Return 1
 }
@@ -124,12 +158,14 @@ else {
 Write-Host "Press Return to continue AFTER Syncthing is setup and sync completes" -ForegroundColor "Red"
 Start-Sleep -Seconds 2
 New-NetFirewallRule -DisplayName "Syncthing" -Direction Inbound -Program "$env:APPDATA\SyncTrayzor\syncthing.exe" -Action Allow | Out-Null
-if (-Not (Get-Process | Where-Object ProcessName -eq "SyncTrayzor")) {
+if(-not (Get-Process | Where-Object ProcessName -eq "SyncTrayzor"))
+{
    Start-Process -Filepath "$env:ProgramFiles\SyncTrayzor\SyncTrayzor.exe" | Out-Null
    Start-Sleep -Seconds 15
 }
 
-if ($SECRET_SYNCTHING_MASTER_DEVICE_ID -and $SECRET_SYNCTHING_MASTER_FOLDER_ID) {
+if($SECRET_SYNCTHING_MASTER_DEVICE_ID -and $SECRET_SYNCTHING_MASTER_FOLDER_ID)
+{
    Start-Process -Filepath "$env:APPDATA\SyncTrayzor\syncthing.exe" -NoNewWindow -Args "cli config devices add --device-id $SECRET_SYNCTHING_MASTER_DEVICE_ID"
    Start-Sleep -Seconds 2
    New-Item -Path "$HOME\.sync" -ItemType Directory -Force | Out-Null
@@ -137,7 +173,8 @@ if ($SECRET_SYNCTHING_MASTER_DEVICE_ID -and $SECRET_SYNCTHING_MASTER_FOLDER_ID) 
 }
 
 Read-Host -Prompt "Is Syncthing setup and sync completed? Press Return to continue"
-if (-Not (Test-Path "$HOME\.sync\.first-sync")) {
+if(-not (Test-Path "$HOME\.sync\.first-sync"))
+{
    Write-Host "!! Exiting: Syncthing is not setup !!" -ForegroundColor "Red"
    Return 1
 }
@@ -189,7 +226,8 @@ Remove-Item -Path "C:\git.inf" -Force
 RefreshEnv
 
 # Squelch git 2.x warning message when pushing
-if (-Not (git config --system push.default)) {
+if(-not (git config --system push.default))
+{
    git config --system push.default simple
 }
 
@@ -207,25 +245,30 @@ Get-Service ssh-agent | Set-Service -StartupType Automatic
 Start-Service ssh-agent
 
 # Update built-in help
-# Write-Host "Updating PowerShell help..." -ForegroundColor "Green"
-# Update-Help -UICulture en-US -Force -ErrorAction SilentlyContinue | Out-Null
+Write-Host "Updating PowerShell help..." -ForegroundColor "Green"
+Update-Help -UICulture en-US -Force -ErrorAction SilentlyContinue | Out-Null
 
 # Remove installed Apps that came back after Windows updated
-if([System.Environment]::OSVersion.Version.Build -lt 22000) {
-   if (Test-Path "$HOME\.dotfiles\bin\remove-win10apps.ps1") {
-       & "$HOME\.dotfiles\bin\remove-win10apps.ps1"
+if(IsWindows10)
+{
+   if(Test-Path "$HOME\.dotfiles\bin\remove-win10apps.ps1")
+   {
+      & "$HOME\.dotfiles\bin\remove-win10apps.ps1"
    }
 }
-else {
-   if (Test-Path "$HOME\.dotfiles\bin\remove-win11apps.ps1") {
-       & "$HOME\.dotfiles\bin\remove-win11apps.ps1"
+else
+{
+   if(Test-Path "$HOME\.dotfiles\bin\remove-win11apps.ps1")
+   {
+      & "$HOME\.dotfiles\bin\remove-win11apps.ps1"
    }
 }
 
 # Install Rotz
 Write-Host "Setup Rotz..." -ForegroundColor "Green"
 scoop install 7zip 6>&1 | Out-Null
-if (-Not (Test-Path "$HOME\.dotfiles\script\strap-support\rotz-windows.7z")) {
+if(-not (Test-Path "$HOME\.dotfiles\script\strap-support\rotz-windows.7z"))
+{
    Write-Host "!! Exiting: Missing strap support files !!" -ForegroundColor "Red"
    Return 1
 }
@@ -235,4 +278,5 @@ if (-Not (Test-Path "$HOME\.dotfiles\script\strap-support\rotz-windows.7z")) {
 & "$HOME\rotz.exe" install | Out-Null
 & "$HOME\rotz.exe" link -f | Out-Null
 
+# All done!
 Write-Host "Strap-after-setup complete -- Returning to strap..." -ForegroundColor "Green"
