0ba68231f0bc4bd75be05c799c27ccf82f141af7
Adam Yellen <6933682+AdamYellen@users.noreply.github.com>
2026-01-22 10:32:47 -0500

    Bye oh my

--- oh-my-zsh/oh-my-zsh-custom/1password.zsh
+++
@@ -1,4 +0,0 @@
-if [ -d $HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t ]
-then
-   export SSH_AUTH_SOCK=$HOME/Library/Group\ Containers/2BUA8C4S2C.com.1password/t/agent.sock
-fi
--- oh-my-zsh/oh-my-zsh-custom/miniconda.zsh
+++
@@ -1,4 +0,0 @@
-if [ -x /opt/homebrew/bin/conda ]
-then
-   eval "$(/opt/homebrew/bin/conda "shell.$(basename "${SHELL}")" hook)"
-fi
